c930103fdc9e8de1fdd3994e5253250ed2b8c688
jwmasekre <josh.masek@joshuamasek.com>
2025-01-26 22:47:20 -0600

    first sveltekit integration

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { goto } from '$app/navigation';
+</script>
+
+<h1>Fallout</h1>
+
+<div class="button-container">
+    <button on:click={() => goto('./builder')}>New</button>
+    <button on:click={() => goto('./loader')}>Load</button>
+    <button on:click={() => goto('./login')}>Login</button>
+    <button on:click={() => goto('./register')}>Register</button>
+</div>
+
